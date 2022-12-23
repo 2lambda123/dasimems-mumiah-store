9d80c69cc96ba11bf54f26b951b3fa9e3a96b966
greatkene <great.ihevueme@tekparadise.com>
2022-12-23 02:44:42 +0100

    contact us

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import { Home, About, Error, SingleProducts } from "./pages"
+import { Home, About, Error, SingleProducts, Contact } from "./pages"
 
 function App(props) {
   return (
@@ -8,8 +8,9 @@ function App(props) {
      <Routes>
        <Route path='/' element={<Home />} />
        <Route path='/about' element={<About />} />
-       <Route path='*' element={<Error />}  />
+       <Route path='/contact' element={<Contact />} />
        <Route path='/products/:id' element={<SingleProducts />} />
+       <Route path='*' element={<Error />}  />
      </Routes>
    </Router>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import Home from "./Home";
 import About from "./About";
 import Error from "./Error";
 import SingleProducts from "./SingleProducts";
+import Contact from "./Contact";
 
 export {
     Home,
     About,
     Error,
-    SingleProducts
+    SingleProducts,
+    Contact
 }

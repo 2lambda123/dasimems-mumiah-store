7cc018391be01ae90a2b0a242e0b87c87afd18fd
greatkene <great.ihevueme@tekparadise.com>
2022-12-31 22:15:47 +0100

    chore

--- src/pages/SingleProducts.jsx
+++ src/pages/SingleProducts.jsx
@@ -1,11 +1,11 @@
-import React, { useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
-import { useProductsContext } from '../contexts/products_context';
-import { formatPrice } from '../utils/helpers';
-import { Link } from 'react-router-dom';
-import { productUrl as url } from '../utils/constant';
-import { Loading, Error, SingleAddToCart } from '../components';
-import {Row, Col, Button, Breadcrumb} from "antd"
+import React, { useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { useProductsContext } from "../contexts/products_context";
+import { formatPrice } from "../utils/helpers";
+import { Link } from "react-router-dom";
+import { productUrl as url } from "../utils/constant";
+import { Loading, Error, SingleAddToCart } from "../components";
+import { Row, Col, Button, Breadcrumb } from "antd";
 
 function SingleProducts(props) {
   const { id } = useParams();
@@ -40,47 +40,34 @@ function SingleProducts(props) {
   const { product } = single_product;
 
   return (
-
     <div className="single-product-items">
-
       <Row justify="center" className="single-product-breadcrumb">
         <Col span="22">
           <Breadcrumb separator=">">
             <Breadcrumb.Item>
-
               <Link to="">Home</Link>
-              
             </Breadcrumb.Item>
 
             <Breadcrumb.Item>
-
               <Link to="">Products</Link>
-
             </Breadcrumb.Item>
 
             <Breadcrumb.Item>
-
               <p>{product?.name}</p>
-
             </Breadcrumb.Item>
-
           </Breadcrumb>
-
         </Col>
-
       </Row>
 
       <div className="container">
         <h1>Single Products</h1>
         <h3>{product?.name}</h3>
-        <p>{formatPrice(product?.price.amount)}</p>
+        <p>{formatPrice(product?.price)}</p>
         <h2>{product?.description}</h2>
         <hr />
         {product?.stock > 0 && <SingleAddToCart product={product} />}
       </div>
-
     </div>
-
   );
 }
 

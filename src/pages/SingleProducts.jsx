1b16a8a4c94ee48656be9ec3e4573b5cc8ba7de5
dasimems <isaacseun63@gmail.com>
2023-01-01 17:34:01 +0100

    update: finished the second section inthe single product page

--- src/pages/SingleProducts.jsx
+++ src/pages/SingleProducts.jsx
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 import { productUrl as url } from '../utils/constant';
 import { Loading, Error, SingleAddToCart } from '../components';
 import {Row, Col, Button, Breadcrumb} from "antd"
+import { FaHeart } from 'react-icons/fa';
 
 function SingleProducts(props) {
   const { id } = useParams();
@@ -68,6 +69,70 @@ function SingleProducts(props) {
 
         </Col>
 
+      </Row>
+
+      <Row justify="center" className="product-content">
+        <Col span={22} className="">
+
+          <Row justify="space-between">
+
+            <Col span={11} className="single-product-image">
+
+
+            </Col>
+
+            <Col span={11} className="single-product-details">
+
+              <h1 className="single-product-title">{product?.name}</h1>
+              <p className="single-product-subtitle">Brand : SKMEImore</p>
+
+              <p className="reviews">(139 reviews)</p>
+
+              <div className="price flex-container align-end">
+                <p className="original-price">{formatPrice(product?.price.amount)}</p>
+                <p className="discounted-price">$75.0</p>
+              </div>
+
+              <p classNmae="single-product-description">{product?.description}</p>
+
+              <div className="single-product-actions  flex-container">
+
+                <div className="quantity-container flex-container">
+
+                  <p>Quantity</p>
+
+                  {product?.stock > 0 && <SingleAddToCart product={product} />}
+
+                </div>
+
+                <Button className="add-to-favorite">
+                  <FaHeart />
+                </Button>
+
+                <div className="extra-details">
+                  <p className="title">Availability</p>
+                  <p className="Value">24 In stock</p>
+                </div>
+
+                <div className="extra-details">
+                  <p className="title">Available sizes</p>
+                  <p className="Value">
+                    <span className="inner-value">XL</span>
+                    <span className="inner-value">S</span>
+                    <span className="inner-value">M</span>
+                    <span className="inner-value">L</span>
+                  </p>
+                </div>
+
+
+              </div>
+
+              
+            </Col>
+            
+          </Row>
+
+        </Col>
       </Row>
 
       <div className="container">

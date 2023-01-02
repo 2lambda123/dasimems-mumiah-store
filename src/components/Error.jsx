28d33c9dd87515d67c3ee6d66f16a49fa33a421a
dasimems <isaacseun63@gmail.com>
2023-01-02 04:36:28 +0100

    feat: added the something went wrong animation

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import { Player } from "@lottiefiles/react-lottie-player";
+import React from "react";
+import ErrorAnim from "../assets/lotties/unknown_error.json"
+import "../assets/styles/error/error.css";
+import {AiOutlineReload} from "react-icons/ai"
 
 function Error(props) {
+
   return (
-    <div>
-      <h1>There was an error</h1>
+    <div className="page-error">
+
+      <Player 
+        src={ErrorAnim}
+        autoplay
+        loop
+        className="page-error-anim"
+      
+      />
+      <h1 className="error-title normal-title">An unknown Error Occurred!</h1>
+      <button className="reload-button" onClick={()=>{
+        window.location.reload();
+
+      }}>Reload &nbsp; <AiOutlineReload /></button>
     </div>
   );
 }

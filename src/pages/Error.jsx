4f1b7527e9f4c1bbcaa2d855af62c72ffa1eb6ce
dasimems <isaacseun63@gmail.com>
2023-01-02 03:47:53 +0100

    feat: did the page not found error

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,9 +1,22 @@
+import { Player } from "@lottiefiles/react-lottie-player";
 import React from "react";
+import PageNotFoundErr from "../assets/lotties/404.json"
+import "../assets/styles/error/error.css";
+import {Link} from "react-router-dom";
 
 function Error(props) {
   return (
-    <div>
-      <h1>An Error Occured</h1>
+    <div className="page-error">
+
+      <Player 
+        src={PageNotFoundErr}
+        autoplay
+        loop
+        className="page-error-anim"
+      
+      />
+      <h1 className="error-title">Page Not Found!</h1>
+      <Link to="/" className="error-link">Back to home</Link>
     </div>
   );
 }

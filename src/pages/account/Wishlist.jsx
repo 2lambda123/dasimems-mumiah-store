d9790341e0fb445dac6d5d95da680ddfc4bed714
dasimems <isaacseun63@gmail.com>
2023-01-08 22:03:11 +0100

    update: finished the wishlist page

--- src/pages/account/Wishlist.jsx
+++ src/pages/account/Wishlist.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { ProductListing } from '../../components'
+import { useFilterContext } from '../../contexts/filter_context';
 
 const Wishlist = () => {
+    
+  const { filtered_products: products } = useFilterContext();
   return (
     <div className="wishlist">
 
         <h1 className="title">List of Saved Products</h1>
+
+        <ProductListing data={products?.slice(0, 12)} />
       
     </div>
   )

b359c025270f6a54caed08a1bc40eb46ded1be73
dasimems <isaacseun63@gmail.com>
2023-01-12 08:31:05 +0100

    update: made update to the product card

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -1,39 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { formatPrice } from "../../utils/helpers";
-import { AiOutlinePlus } from "react-icons/ai";
-import { FaShoppingCart } from "react-icons/fa";
 
-function ProductCard({ image, name, price, id, className }) {
+function ProductCard({ image, name, price, category: {name: categoryName}, id, sizes, className, ...props }) {
+  
   return (
     <div key={id} className="col-sm-12 col-md-12 col-lg-12 d-flex">
       <div className="product">
         <Link to={`/products/${id}`} className="img-prod">
-          <img className="img-fluid" src={image} alt={name} />
+          <img className="img-fluid product-image" src={image} alt={name} />
         </Link>
 
-        <div className="text py-3 pb-4 px-3">
-          <h3>
-            <a href="#">{name}</a>
-          </h3>
-          <div className="pricing">
-            <p className="price">
-              <span>{formatPrice(price)}</span>
-            </p>
+        <div className="product-details py-3 pb-4 px-3">
+
+          <div className="small-images-container">
+
+            <div className="images-content flex-container">
+
+              <div className="small-images flex-container  align-center justify-center"></div>
+              <div className="small-images flex-container  align-center justify-center"></div>
+
+            </div>
+
           </div>
-          <p className="bottom-area d-flex px-3">
-            <a href="#" className="add-to-cart text-center py-2 mr-1">
-              <span>
-                Add to cart <AiOutlinePlus className="ml-1" />
-              </span>
-            </a>
-            <a href="#" className="buy-now text-center py-2">
-              Buy now
-              <span>
-                <FaShoppingCart className="ml-1" />
-              </span>
-            </a>
+
+          <h3>
+            <a href="#prod">{name.length > 20? `${name.slice(0, 20)}...`: name}</a>
+          </h3>
+          <p className="product-category">{categoryName}</p>
+          <p className="product-size-stat">{sizes.length} available Sizes</p>
+          <p className="price">
+            <span>{formatPrice(price)}</span>
+            {/* <del>400</del> */}
           </p>
+
+          <p className="discount">14% off</p>
+          
         </div>
       </div>
     </div>

34797d51553241174091f5110d96033b4b07ea73
greatkene <ihevuemeg@gmail.com>
2023-01-14 12:07:47 +0100

    feat: acct update

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,37 +1,34 @@
-import React from 'react'
-import {Row, Col} from "antd"
-import { AccountHeaderNav } from '../components'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import { Row, Col } from "antd";
+import { AccountHeaderNav } from "../components";
+import { Outlet } from "react-router-dom";
 
 const Account = () => {
+  const name = localStorage.getItem("userName");
+  const email = localStorage.getItem("userEmail");
+
   return (
     <>
-
       <Row justify="center" className="account">
         <Col span={21} className="account-container">
-
           <h1 className="title">Account</h1>
 
-          <p className='subtitle'>
-            <span className='bold'>Enrico Cole,</span>
-            <span className='light'>ciseco@gmail.com</span>
-            <span className='dot'>.</span>
-            <span className='light'>Los Angeles, CA</span>
+          <p className="subtitle">
+            <span className="bold">{name},</span>
+            <span className="light">{email}</span>
+            <span className="dot">.</span>
+            <span className="light">Los Angeles, CA</span>
           </p>
 
           <AccountHeaderNav />
 
-          <div className='account-contents'>
-
+          <div className="account-contents">
             <Outlet />
-
           </div>
-
         </Col>
       </Row>
-    
     </>
-  )
-}
+  );
+};
 
-export default Account
+export default Account;

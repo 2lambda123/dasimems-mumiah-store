a7199eff5870546ba2c6a08ad4c61c29208d2ba3
greatkene <ihevuemeg@gmail.com>
2023-01-14 23:55:51 +0100

    update: post account info

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import axios from "axios";
 import { useForm } from "react-hook-form";
 import { Col, Row } from "antd";
 import { useNavigate } from "react-router-dom";

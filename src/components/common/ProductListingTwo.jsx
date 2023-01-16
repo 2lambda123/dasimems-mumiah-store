b59a9363ca1122a5df532ebbe2a503e324e7c1c3
greatkene <ihevuemeg@gmail.com>
2023-01-16 17:07:01 +0100

    chore

--- src/components/common/ProductListingTwo.jsx
+++ src/components/common/ProductListingTwo.jsx
@@ -1,35 +1,26 @@
-import React from 'react'
-import {Row, Col}from "antd";
-import ProductCard from '../products/ProductCard';
+import React from "react";
+import { Row, Col } from "antd";
+import ProductCard from "../products/ProductCard";
 
-const ProductListingTwo = ({data}) => {
+const ProductListingTwo = ({ data }) => {
   return (
-    <Row
-        justify={"space-between"}
-    >
-
+    <Row justify={"space-between"}>
       {data.map((product, index) => {
-
         return (
-            <Col 
-                key={index}
-                span={7}
-                lg={{span: 7}}
-                md={{span: 7}}
-                xs={{span: 11}}
-                className='product-card'
-            >
-                <ProductCard 
-                    key={product.id}
-                    {...product} 
-                />
-
-            </Col>
-            
-        )
+          <Col
+            key={index}
+            span={7}
+            lg={{ span: 7 }}
+            md={{ span: 7 }}
+            xs={{ span: 11 }}
+            className="product-card"
+          >
+            <ProductCard key={product.id} {...product} />
+          </Col>
+        );
       })}
     </Row>
-  )
-}
+  );
+};
 
-export default ProductListingTwo
+export default ProductListingTwo;

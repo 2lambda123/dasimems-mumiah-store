aed698c2ec0af9578fec96500880a2fcc81f6d4d
greatkene <ihevuemeg@gmail.com>
2023-01-28 11:49:18 +0100

    feat: context for user details

--- src/contexts/products_context.js
+++ src/contexts/products_context.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-import React, { useContext, useReducer, useEffect } from 'react'
+import React, { useContext, useReducer, useEffect } from "react";
 
-import reducer from "../reducers/product_reducer"
-import { productUrl as url } from "../utils/constant";
+import reducer from "../reducers/product_reducer";
+import { productUrl as url, baseUrl } from "../utils/constant";
 
 import {
   SIDEBAR_OPEN,
@@ -16,79 +16,110 @@ import {
   GET_SINGLE_PRODUCT_BEGIN,
   GET_SINGLE_PRODUCT_SUCCESS,
   GET_SINGLE_PRODUCT_ERROR,
-} from "../_actions"
+  USER_DETAILS,
+  USER_DETAILS_SUCCESS,
+  USER_DETAILS_ERROR,
+} from "../_actions";
 
 const initialState = {
-    isSidebarOpen: false,
-    products_loading: true,
-    products_error: false,
-    products: [],
-    category: [],
-    cat_loading: true,
-    cat_error: false,
-    single_product_loading: true,
-    single_product_error: false,
-    single_product: {},
-}
+  isSidebarOpen: false,
+  products_loading: true,
+  products_error: false,
+  products: [],
+  category: [],
+  cat_loading: true,
+  cat_error: false,
+  single_product_loading: true,
+  single_product_error: false,
+  single_product: {},
+  user_details: [],
+  user_details_loading: true,
+  user_details_error: false,
+};
 
-const ProductsContext = React.createContext()
+const ProductsContext = React.createContext();
 
 export const ProductsProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, initialState)
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const openSidebar = () => {
-    dispatch({type: SIDEBAR_OPEN})
-  }
-  
+    dispatch({ type: SIDEBAR_OPEN });
+  };
+
   const closeSidebar = () => {
-    dispatch({type: SIDEBAR_CLOSE})
-  }
+    dispatch({ type: SIDEBAR_CLOSE });
+  };
 
-    const fetchProducts = async (url) => {
-        dispatch({type: GET_PRODUCTS_BEGIN})
-        try {
-          const response = await axios.get(url)
-          const products = response.data.products
-          dispatch({type: GET_PRODUCTS_SUCCESS, payload: products})
-        } catch (error) {
-          dispatch({type: GET_PRODUCTS_ERROR})
-        } 
+  const fetchProducts = async (url) => {
+    dispatch({ type: GET_PRODUCTS_BEGIN });
+    try {
+      const response = await axios.get(url);
+      const products = response.data.products;
+      dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products });
+    } catch (error) {
+      dispatch({ type: GET_PRODUCTS_ERROR });
     }
+  };
 
-    const fetchCategories = async (url) => {
-        dispatch({type: GET_CAT_BEGIN})
-        try {
-          const response = await axios.get(`${url}/categories`)
-          const category = response.data
-          dispatch({type: GET_CAT_SUCCESS, payload: category})
-        } catch (error) {
-          dispatch({type: GET_CAT_ERROR})
-        } 
+  const fetchCategories = async (url) => {
+    dispatch({ type: GET_CAT_BEGIN });
+    try {
+      const response = await axios.get(`${url}/categories`);
+      const category = response.data;
+      dispatch({ type: GET_CAT_SUCCESS, payload: category });
+    } catch (error) {
+      dispatch({ type: GET_CAT_ERROR });
     }
-      
-    const fetchSingleProduct = async(url) => {
-        dispatch({type: GET_SINGLE_PRODUCT_BEGIN})
-        try {
-          const response = await axios.get(url);
-          const singleProduct = response.data
-          dispatch({type: GET_SINGLE_PRODUCT_SUCCESS, payload: singleProduct})
-        } catch (error) {
-          dispatch({type: GET_SINGLE_PRODUCT_ERROR})
-        }
-      }
+  };
 
-    useEffect(() => {
-        fetchProducts(url)
-        fetchCategories(url)
-    }, [])
+  const fetchSingleProduct = async (url) => {
+    dispatch({ type: GET_SINGLE_PRODUCT_BEGIN });
+    try {
+      const response = await axios.get(url);
+      const singleProduct = response.data;
+      dispatch({ type: GET_SINGLE_PRODUCT_SUCCESS, payload: singleProduct });
+    } catch (error) {
+      dispatch({ type: GET_SINGLE_PRODUCT_ERROR });
+    }
+  };
 
-    return (
-        <ProductsContext.Provider value={{ ...state, openSidebar, closeSidebar, fetchSingleProduct }}>
-            {children}
-        </ProductsContext.Provider>
-    )
-}
+  const fetchUserDetails = async (baseUrl) => {
+    dispatch({ type: USER_DETAILS });
+    try {
+      const response = await axios.get(`${baseUrl}/account`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("user_token")}`,
+        },
+      });
+      const user_details = response.data;
+      console.log(user_details);
+      dispatch({ type: USER_DETAILS_SUCCESS, payload: user_details });
+    } catch (error) {
+      dispatch({ type: USER_DETAILS_ERROR });
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts(url);
+    fetchCategories(url);
+    fetchUserDetails(baseUrl);
+  }, []);
+
+  return (
+    <ProductsContext.Provider
+      value={{
+        ...state,
+        openSidebar,
+        closeSidebar,
+        fetchSingleProduct,
+        fetchUserDetails,
+      }}
+    >
+      {children}
+    </ProductsContext.Provider>
+  );
+};
 
 export const useProductsContext = () => {
-    return useContext(ProductsContext)
-}
+  return useContext(ProductsContext);
+};
--- src/reducers/product_reducer.js
+++ src/reducers/product_reducer.js
@@ -1,79 +1,93 @@
 import {
-    SIDEBAR_OPEN,
-    SIDEBAR_CLOSE,
-    GET_PRODUCTS_BEGIN,
-    GET_PRODUCTS_ERROR,
-    GET_PRODUCTS_SUCCESS,
-    GET_CAT_BEGIN,
-    GET_CAT_SUCCESS,
-    GET_CAT_ERROR,
-    GET_SINGLE_PRODUCT_BEGIN,
-    GET_SINGLE_PRODUCT_SUCCESS,
-    GET_SINGLE_PRODUCT_ERROR,
-} from "../_actions"
+  SIDEBAR_OPEN,
+  SIDEBAR_CLOSE,
+  GET_PRODUCTS_BEGIN,
+  GET_PRODUCTS_ERROR,
+  GET_PRODUCTS_SUCCESS,
+  GET_CAT_BEGIN,
+  GET_CAT_SUCCESS,
+  GET_CAT_ERROR,
+  GET_SINGLE_PRODUCT_BEGIN,
+  GET_SINGLE_PRODUCT_SUCCESS,
+  GET_SINGLE_PRODUCT_ERROR,
+  USER_DETAILS,
+  USER_DETAILS_SUCCESS,
+  USER_DETAILS_ERROR,
+} from "../_actions";
 
 const products_reducer = (state, action) => {
-    
-    if( action.type === SIDEBAR_OPEN ) {
-        return {...state, isSidebarOpen: true}
-    }
-    if( action.type === SIDEBAR_CLOSE ) {
-        return {...state, isSidebarOpen: false}
-    }
+  if (action.type === SIDEBAR_OPEN) {
+    return { ...state, isSidebarOpen: true };
+  }
+  if (action.type === SIDEBAR_CLOSE) {
+    return { ...state, isSidebarOpen: false };
+  }
 
-    if( action.type === GET_PRODUCTS_BEGIN ) {
-        return {...state, products_loading: true}
-    }
-    if(action.type === GET_PRODUCTS_SUCCESS) {
-        return {
-          ...state,
-          products_loading: false,
-          products: action.payload,
-        }
-    }
-    if(action.type === GET_PRODUCTS_ERROR) {
-        return { ...state, products_loading: false, products_error: true }
-    }
+  if (action.type === GET_PRODUCTS_BEGIN) {
+    return { ...state, products_loading: true };
+  }
+  if (action.type === GET_PRODUCTS_SUCCESS) {
+    return {
+      ...state,
+      products_loading: false,
+      products: action.payload,
+    };
+  }
+  if (action.type === GET_PRODUCTS_ERROR) {
+    return { ...state, products_loading: false, products_error: true };
+  }
 
+  if (action.type === GET_CAT_BEGIN) {
+    return { ...state, cat_loading: true };
+  }
+  if (action.type === GET_CAT_SUCCESS) {
+    return {
+      ...state,
+      cat_loading: false,
+      category: action.payload,
+    };
+  }
+  if (action.type === GET_CAT_ERROR) {
+    return { ...state, cat_loading: false, cat_error: true };
+  }
 
-    if( action.type === GET_CAT_BEGIN ) {
-        return {...state, cat_loading: true}
-    }
-    if(action.type === GET_CAT_SUCCESS) {
-        return {
-          ...state,
-          cat_loading: false,
-          category: action.payload,
-        }
-    }
-    if(action.type === GET_CAT_ERROR) {
-        return { ...state, cat_loading: false, cat_error: true }
-    }
+  if (action.type === GET_SINGLE_PRODUCT_BEGIN) {
+    return {
+      ...state,
+      single_product_loading: true,
+      single_product_error: false,
+    };
+  }
+  if (action.type === GET_SINGLE_PRODUCT_SUCCESS) {
+    return {
+      ...state,
+      single_product_loading: false,
+      single_product: action.payload,
+    };
+  }
+  if (action.type === GET_SINGLE_PRODUCT_ERROR) {
+    return {
+      ...state,
+      single_product_loading: false,
+      single_product_error: true,
+    };
+  }
 
-    
-    if(action.type === GET_SINGLE_PRODUCT_BEGIN) {
-        return {
-          ...state, 
-          single_product_loading: true, 
-          single_product_error: false
-        }
-    }
-    if(action.type === GET_SINGLE_PRODUCT_SUCCESS) {
-        return {
-          ...state, 
-          single_product_loading:false, 
-          single_product: action.payload
-        }
-    }
-    if(action.type === GET_SINGLE_PRODUCT_ERROR) {
-        return {
-          ...state, 
-          single_product_loading: false, 
-          single_product_error: true
-        }
-    }
-  
-    throw new Error(`No Matching "${action.type}" - action type`)
-}
+  if (action.type === USER_DETAILS) {
+    return { ...state, products_loading: true };
+  }
+  if (action.type === USER_DETAILS_SUCCESS) {
+    return {
+      ...state,
+      user_details_loading: false,
+      user_details: action.payload,
+    };
+  }
+  if (action.type === USER_DETAILS_ERROR) {
+    return { ...state, user_details_loading: false, user_details_error: true };
+  }
 
-export default products_reducer
+  throw new Error(`No Matching "${action.type}" - action type`);
+};
+
+export default products_reducer;
